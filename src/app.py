c04e46d65d6d1f27b811f6e1f79c4e7632e9ff1f
Alberto Alcantara <jalcantara@vsys.com>
2022-02-17 15:10:23 -0600

    segundo commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
-from flask_cors import CORS, cross_origin
+#from flask_cors import CORS, cross_origin
 
 from config import config
 from validaciones import *
@@ -8,109 +8,78 @@ from validaciones import *
 app = Flask(__name__)
 
 # CORS(app)
-CORS(app, resources={r"/cursos/*": {"origins": "http://localhost"}})
+#CORS(app, resources={r"/*": {"origins": "http://127.0.0.1"}})
 
 conexion = MySQL(app)
 
 
-# @cross_origin
-@app.route('/cursos', methods=['GET'])
+# Get a list of users
+@app.route('/users', methods=['GET'])
 def listar_cursos():
     try:
         cursor = conexion.connection.cursor()
-        sql = "SELECT codigo, nombre, creditos FROM curso ORDER BY nombre ASC"
+        sql = "SELECT * FROM usuarios ORDER BY nombre ASC"
         cursor.execute(sql)
-        datos = cursor.fetchall()
-        cursos = []
-        for fila in datos:
-            curso = {'codigo': fila[0], 'nombre': fila[1], 'creditos': fila[2]}
-            cursos.append(curso)
-        return jsonify({'cursos': cursos, 'mensaje': "Cursos listados.", 'exito': True})
+        data = cursor.fetchall()
+        users = []
+        for row in data:
+            user = {'id':row[6],'nombre':row[0], 'apellido':row[1], 'telefono': row[2],"correo":row[3],"edad":row[4],"estado":row[5]}
+            users.append(user)
+        return jsonify({'users': users, 'mensaje': "Users List", 'exito': True})
     except Exception as ex:
         return jsonify({'mensaje': "Error", 'exito': False})
 
 
-def leer_curso_bd(codigo):
+#add a new user
+@app.route('/add', methods=['POST'])
+def addUser():
+    print(request)
     try:
-        cursor = conexion.connection.cursor()
-        sql = "SELECT codigo, nombre, creditos FROM curso WHERE codigo = '{0}'".format(codigo)
-        cursor.execute(sql)
-        datos = cursor.fetchone()
-        if datos != None:
-            curso = {'codigo': datos[0], 'nombre': datos[1], 'creditos': datos[2]}
-            return curso
-        else:
-            return None
-    except Exception as ex:
-        raise ex
-
-
-@app.route('/cursos/<codigo>', methods=['GET'])
-def leer_curso(codigo):
-    try:
-        curso = leer_curso_bd(codigo)
+        curso =None
         if curso != None:
             return jsonify({'curso': curso, 'mensaje': "Curso encontrado.", 'exito': True})
         else:
-            return jsonify({'mensaje': "Curso no encontrado.", 'exito': False})
-    except Exception as ex:
-        return jsonify({'mensaje': "Error", 'exito': False})
-
-
-@app.route('/cursos', methods=['POST'])
-def registrar_curso():
-    # print(request.json)
-    if (validar_codigo(request.json['codigo']) and validar_nombre(request.json['nombre']) and validar_creditos(request.json['creditos'])):
-        try:
-            curso = leer_curso_bd(request.json['codigo'])
-            if curso != None:
-                return jsonify({'mensaje': "Código ya existe, no se puede duplicar.", 'exito': False})
-            else:
-                cursor = conexion.connection.cursor()
-                sql = """INSERT INTO curso (codigo, nombre, creditos) 
-                VALUES ('{0}', '{1}', {2})""".format(request.json['codigo'],
-                                                     request.json['nombre'], request.json['creditos'])
-                cursor.execute(sql)
-                conexion.connection.commit()  # Confirma la acción de inserción.
-                return jsonify({'mensaje': "Curso registrado.", 'exito': True})
-        except Exception as ex:
-            return jsonify({'mensaje': "Error", 'exito': False})
-    else:
-        return jsonify({'mensaje': "Parámetros inválidos...", 'exito': False})
-
-
-@app.route('/cursos/<codigo>', methods=['PUT'])
-def actualizar_curso(codigo):
-    if (validar_codigo(codigo) and validar_nombre(request.json['nombre']) and validar_creditos(request.json['creditos'])):
-        try:
-            curso = leer_curso_bd(codigo)
-            if curso != None:
-                cursor = conexion.connection.cursor()
-                sql = """UPDATE curso SET nombre = '{0}', creditos = {1} 
-                WHERE codigo = '{2}'""".format(request.json['nombre'], request.json['creditos'], codigo)
-                cursor.execute(sql)
-                conexion.connection.commit()  # Confirma la acción de actualización.
-                return jsonify({'mensaje': "Curso actualizado.", 'exito': True})
-            else:
-                return jsonify({'mensaje': "Curso no encontrado.", 'exito': False})
-        except Exception as ex:
-            return jsonify({'mensaje': "Error", 'exito': False})
-    else:
-        return jsonify({'mensaje': "Parámetros inválidos...", 'exito': False})
-
-
-@app.route('/cursos/<codigo>', methods=['DELETE'])
-def eliminar_curso(codigo):
-    try:
-        curso = leer_curso_bd(codigo)
-        if curso != None:
+            print("Estoy dentro del else")
             cursor = conexion.connection.cursor()
-            sql = "DELETE FROM curso WHERE codigo = '{0}'".format(codigo)
-            cursor.execute(sql)
-            conexion.connection.commit()  # Confirma la acción de eliminación.
-            return jsonify({'mensaje': "Curso eliminado.", 'exito': True})
-        else:
-            return jsonify({'mensaje': "Curso no encontrado.", 'exito': False})
+            sql = """INSERT INTO usuarios (nombre, apellido, telefono, correo, edad, estado) 
+            VALUES ('{}', '{}', '{}','{}',{},'{}')""".format(request.json['nombre'],
+            request.json['apellido'], request.json['telefono'],request.json['correo'],request.json['edad'],request.json['estado'])
+
+            print(cursor.execute(sql))
+            conexion.connection.commit()  # Confirma la acción de inserción.
+            return jsonify({'mensaje': "Curso registrado.", 'exito': True})
+           
+    except Exception as ex:
+        return jsonify({'mensaje': ex, 'exito': False})
+
+
+
+#Update user
+@app.route('/user/<id>', methods=['PUT'])
+def updateUser(id):
+        try:
+            cursor = conexion.connection.cursor()
+            sql = """UPDATE usuarios SET nombre = '{}', apellido = '{}',telefono='{}',correo='{}',edad={}, estado='{}' 
+            WHERE id = '{}'""".format(request.json['nombre'],request.json['apellido'],request.json['telefono'],request.json['correo'],request.json['edad'],request.json['estado'],id )
+            print(cursor.execute(sql))
+            conexion.connection.commit()  # Confirma la acción de actualización.
+            return jsonify({'mensaje': "User Update", 'exito': True})
+    
+        except Exception as ex:
+            return jsonify({'mensaje': ex, 'exito': False})
+    
+    
+
+#Delete user
+@app.route('/delete/<id>', methods=['DELETE'])
+def deleteUSer(id):
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "DELETE FROM usuarios WHERE id = '{}'".format(id)
+        cursor.execute(sql)
+        conexion.connection.commit()  
+        return jsonify({'mensaje': "deleted user.", 'exito': True})
+
     except Exception as ex:
         return jsonify({'mensaje': "Error", 'exito': False})
 
--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'S3rv3r'
-    MYSQL_DB = 'api_flask'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'usuarios'
 
 
 config = {
